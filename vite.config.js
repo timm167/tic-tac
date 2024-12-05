1e6612133a4a0abdb9151806c5d9757e34a4b358
timm167 <tim.charterii@gmail.com>
2024-12-05 19:11:12 +0000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/timm167/tic-tac'
+  base: '/timm167/tic-tac/'
 })
